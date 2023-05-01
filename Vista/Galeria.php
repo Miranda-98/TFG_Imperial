17646ddd273c4399062faff92980f929dd519162
Miranda-98 <gerardomiranda2498@gmail.com>
2023-05-01 19:50:44 +0200

    galeria

--- Vista/Galeria.php
+++ Vista/Galeria.php
@@ -9,18 +9,113 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-   
+
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="mediaQuery.css">
+    <link rel="stylesheet" type="text/css" href="galeria.css">
+
 </head>
 
 <body>
-    <h1>PAGINA DE GALERIA DE FOTOS </h1>
+    <h1>PÁGINA DE GALERIA DE FOTOS ROBADAS</h1>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#colapse1" aria-expanded="true" aria-controls="colapse1">
+                    Espacios Comunes
+                </button>
+            </h2>
+            <div id="colapse1" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <!-- Comienzo galeria de fotos -->
+                    <div class="contenedor containerCollageFotos">
+                        <div class="item foto1"><img src="../img/fotos/balcon_hd.jpg"></div>
+                        <div class="item foto2"><img src="../img/fotos/restaurantete.jpeg"></div>
+                        <div class="item foto3"><img src="../img/fotos/playa_hd.jpg"></div>
+                        <div class="item foto4"><img src="../img/fotos/instalacion1.jpg"></div>
+                        <div class="item foto5"><img src="../img/fotos/instalacion3.jpg"></div>
+                        <div class="item foto6"><img src="../img/fotos/instalacion2.jpg"></div>
+                        <div class="item foto7"><img src="../img/fotos/comida.jpeg"></div>
+                        <div class="item foto8"><img src="../img/fotos/jardin.jpg"></div>
+                        <div class="item foto9"><img src="../img/fotos/terraza_completo.jpg"></div>
+                        <div class="item foto10"><img src="../img/fotos/baño_vertical.jpg"></div>
+                        <div class="item foto11"><img src="../img/fotos/baño_completo.jpg"></div>
+                        <div class="item foto12"><img src="../img/fotos/baño_horizontal.jpg"></div>
+                        <div class="item foto13"><img src="../img/fotos/baño_horizontal.jpg"></div>
+                    </div>
+                    <!-- Fin galeria de fotos -->
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#colapse2" aria-expanded="false" aria-controls="colapse2">
+                    Habitaciones
+                </button>
+            </h2>
+            <div id="colapse2" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <!-- Comienzo galeria de fotos -->
+                    <h1>Presidencial</h1>
+                    <div class="contenedor containerFotosHabitacionPresidencial">
+                        <div class="item foto1"><img src="../img/habitaciones/hab_presidencial/foto1.jpg"></div>
+                        <div class="item foto2"><img src="../img/habitaciones/hab_presidencial/foto2.jpg"></div>
+                        <div class="item foto3"><img src="../img/habitaciones/hab_presidencial/foto3.jpg"></div>
+                        <div class="item foto4"><img src="../img/habitaciones/hab_presidencial/foto4.jpg"></div>
+                        <div class="item foto5"><img src="../img/habitaciones/hab_presidencial/foto5.jpg"></div>
+                        <div class="item foto6"><img src="../img/habitaciones/hab_presidencial/foto6.jpg"></div>
+                        <div class="item foto7"><img src="../img/habitaciones/hab_presidencial/foto7.jpg"></div>
+                        <div class="item foto8"><img src="../img/habitaciones/hab_presidencial/foto8.jpg"></div>
+                        <div class="item foto9"><img src="../img/habitaciones/hab_presidencial/foto9.jpg"></div>
+                        <div class="item foto10"><img src="../img/habitaciones/hab_presidencial/foto10.jpg"></div>
+                    </div>
+
+                    <br/><br/><br/><h1>Lujo</h1>
+                    <div class="contenedor containerFotosHabitacionLujo">
+                        <div class="item foto1"><img src="../img/habitaciones/hab_lujo/foto1.jpg"></div>
+                        <div class="item foto2"><img src="../img/habitaciones/hab_lujo/foto2.jpg"></div>
+                        <div class="item foto3"><img src="../img/habitaciones/hab_lujo/foto3.jpg"></div>
+                        <div class="item foto4"><img src="../img/habitaciones/hab_lujo/foto4.jpg"></div>
+                        <div class="item foto5"><img src="../img/habitaciones/hab_lujo/foto5.jpg"></div>
+                        <div class="item foto6"><img src="../img/habitaciones/hab_lujo/foto6.jpg"></div>
+                        <div class="item foto7"><img src="../img/habitaciones/hab_lujo/foto7.jpg"></div>
+                        <div class="item foto8"><img src="../img/habitaciones/hab_lujo/foto8.jpg"></div>
+                    </div>
+                    
+                    <br/><br/><br/><h1>Comun</h1>
+                    <div class="contenedor containerFotosHabitacionComun">
+                        <div class="item foto1"><img src="../img/habitaciones/hab_comun/foto1.jpg"></div>
+                        <div class="item foto2"><img src="../img/habitaciones/hab_comun/foto2.jpg"></div>
+                        <div class="item foto3"><img src="../img/habitaciones/hab_comun/foto3.jpg"></div>
+                        <div class="item foto4"><img src="../img/habitaciones/hab_comun/foto4.jpg"></div>
+                        <div class="item foto5"><img src="../img/habitaciones/hab_comun/foto5.jpg"></div>
+                    </div>
+                    <!-- Fin galeria de fotos -->
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#colapse3" aria-expanded="false" aria-controls="colapse3">
+                        Salas de Eventos
+                    </button>
+                </h2>
+                <div id="colapse3" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <!-- Comienzo galeria de fotos -->
+                            
+                        <!-- Fin galeria de fotos -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <h1>PAGINA DE GALERIA DE FOTOS </h1>
     <div class="row">
         <div class="col-md-3">
-            <a href="../img/fotos/instalacion1.jpg" target="_blank">
+            <a href="img/fotos/instalacion1.jpg" target="_blank">
                 <div class="overlay">
-                    <img src="../img/fotos/instalacion1.jpg" alt="Imagen 1" class="img-fluid">
+                    <img src="img/fotos/instalacion1.jpg" alt="Imagen 1" class="img-fluid">
                     <div class="zoom-icon">
                         <i class="fa fa-search-plus" aria-hidden="true"></i>
                     </div>
@@ -30,7 +125,7 @@
         <div class="col-md-3">
             <a href="..img/fotos/instalacion2.jpg" target="_blank">
                 <div class="overlay">
-                    <img src="../img/fotos/instalacion2.jpg" alt="Imagen 2" class="img-fluid">
+                    <img src="img/fotos/instalacion2.jpg" alt="Imagen 2" class="img-fluid">
                     <div class="zoom-icon">
                         <i class="fa fa-search-plus" aria-hidden="true"></i>
                     </div>
@@ -38,9 +133,9 @@
             </a>
         </div>
         <div class="col-md-3">
-            <a href="../img/fotos/instalacion3.jpg" target="_blank">
+            <a href="img/fotos/instalacion3.jpg" target="_blank">
                 <div class="overlay">
-                    <img src="../img/fotos/instalacion3.jpg" alt="Imagen 3" class="img-fluid">
+                    <img src="img/fotos/instalacion3.jpg" alt="Imagen 3" class="img-fluid">
                     <div class="zoom-icon">
                         <i class="fa fa-search-plus" aria-hidden="true"></i>
                     </div>
@@ -48,16 +143,16 @@
             </a>
         </div>
         <div class="col-md-3">
-            <a href="../img/fotos/jardin.jpg" target="_blank">
+            <a href="img/fotos/jardin.jpg" target="_blank">
                 <div class="overlay">
-                    <img src="../img/fotos/jardin.jpg" alt="Imagen 4" class="img-fluid">
+                    <img src="img/fotos/jardin.jpg" alt="Imagen 4" class="img-fluid">
                     <div class="zoom-icon">
                         <i class="fa fa-search-plus" aria-hidden="true"></i>
                     </div>
                 </div>
             </a>
         </div>
-    </div>
+    </div> -->
 
 
 </body>
